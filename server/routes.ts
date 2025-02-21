d8670e398ed65f2f4c2a79d0327b016ed68b0648
Maarten Coppens <maarcopp@student.arteveldehs.be>
2025-02-21 12:09:56 +0100

    Version 1
    In de les

---
+++ server/routes.ts
@@ -0,0 +1,47 @@
+import express, { Request, Response, Router } from "express";
+
+const router: Router = express.Router();
+const newsArticles = [{title: "Donald Trump is gay", description: "Vandaag is donald trump uit de kast gekomen."}, {title: "Barack Obama did a dap", description: "Hij leeft nog zoals hij 16 was."}, {title: "Bavo De Bondt heeft een lange pink", description: "Danne lange hand"}];
+
+const postFetch = async (input: string) => {
+    const response = await fetch("https://api.deweirdt.be/games/index.php", {
+        method: "POST",
+        headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({ gamekey: "Maartens_G4m3", username: input }),
+    })
+}
+
+// Homepagina
+router.get("/", (req: Request, res: Response): void => {
+  res.render("index", { title: "Maarten's Secret Challenge" });
+});
+
+// Quiz verwerken
+router.post("/quiz", (req: Request, res: Response): void => {
+    const correctAntwoord: number = 42;
+    const userAntwoord: number = parseInt(req.body.antwoord?.trim()) || 0;
+    const isCorrect: boolean = userAntwoord === correctAntwoord;
+    const message: string = isCorrect ? "Gefeliciteerd, je hebt het voltooid!" : "Fout! 😢 Probeer opnieuw.";
+
+    const usernameInputValue: string = req.body.username?.trim() || "";    
+    if(isCorrect) {
+        postFetch(usernameInputValue);
+    }
+    
+    res.render("result", { title: "Challenge resultaat", boodschap: message });
+});
+
+router.get("/news", (req: Request, res: Response): void => {
+    res.render("news", { title: "Article", kanker: newsArticles })
+});
+
+//redirect get request op /quiz naar /
+router.get("/quiz", (req: Request, res: Response): void => {
+    res.redirect("/");
+  });
+
+
+
+export default router;
