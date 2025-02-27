62a43ef4cd94ae727c43e93b4ba4015df0393056
Maarten Coppens <maarcopp@student.arteveldehs.be>
2025-02-27 14:17:20 +0100

    Fetch post

--- server/app.ts
+++ server/app.ts
@@ -4,7 +4,7 @@ import expressLayouts from "express-ejs-layouts";
 import routes from "./routes";
 
 const app: Application = express();
-const PORT: number = parseInt(<string>process.env.PORT, 10) || 3000;
+const PORT: number = parseInt(<string>process.env.PORT, 10) || 3001;
 
 // EJS als template-engine instellen
 app.set("view engine", "ejs");
--- server/routes.ts
+++ server/routes.ts
@@ -3,22 +3,6 @@ import express, { Request, Response, Router } from "express";
 const router: Router = express.Router();
 const newsArticles = [{title: "Donald Trump is gay", description: "Vandaag is donald trump uit de kast gekomen."}, {title: "Barack Obama did a dap", description: "Hij leeft nog zoals hij 16 was."}, {title: "Bavo De Bondt heeft een lange pink", description: "Danne lange hand"}];
 
-const postFetch = async (input: string) => {
-    try {
-        const response = await fetch("https://api.deweirdt.be/games/index.php", {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: new URLSearchParams({ gamekey: "Maartens_G4m3", username: input }),
-        });
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-    } catch (error) {
-        console.error("Error posting data:", error);
-    }
-}
 
 // Homepagina
 router.get("/", (req: Request, res: Response): void => {
@@ -31,12 +15,21 @@ router.post("/quiz", (req: Request, res: Response): void => {
     const userAntwoord: number = parseInt(req.body.antwoord?.trim()) || 0;
     const isCorrect: boolean = userAntwoord === correctAntwoord;
     const message: string = isCorrect ? "Gefeliciteerd, je hebt het voltooid!" : "Fout! 😢 Probeer opnieuw.";
-    res.render("result", { title: "Challenge resultaat", boodschap: message });
-
-    const usernameInputValue: string = req.body.username?.trim() || "";    
+    
     if(isCorrect) {
-        postFetch(usernameInputValue);
+        fetch(
+            "https://api.deweirdt.be/games/index.php", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded"
+                },
+                body: new URLSearchParams({username: req.body.username, game_key: "Maartens_G4m3"})
+            })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .then(error => console.error(error));
     }
+    res.render("result", { title: "Challenge resultaat", boodschap: message });
 });
 
 router.get("/news", (req: Request, res: Response): void => {
