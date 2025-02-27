23579f623f021815198ca6bb2c9cf39c83be468a
Maarten Coppens <maarcopp@student.arteveldehs.be>
2025-02-27 13:00:40 +0100

    Vercel

--- server/routes.ts
+++ server/routes.ts
@@ -4,13 +4,20 @@ const router: Router = express.Router();
 const newsArticles = [{title: "Donald Trump is gay", description: "Vandaag is donald trump uit de kast gekomen."}, {title: "Barack Obama did a dap", description: "Hij leeft nog zoals hij 16 was."}, {title: "Bavo De Bondt heeft een lange pink", description: "Danne lange hand"}];
 
 const postFetch = async (input: string) => {
-    const response = await fetch("https://api.deweirdt.be/games/index.php", {
-        method: "POST",
-        headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: new URLSearchParams({ gamekey: "Maartens_G4m3", username: input }),
-    })
+    try {
+        const response = await fetch("https://api.deweirdt.be/games/index.php", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams({ gamekey: "Maartens_G4m3", username: input }),
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+    } catch (error) {
+        console.error("Error posting data:", error);
+    }
 }
 
 // Homepagina
@@ -24,13 +31,12 @@ router.post("/quiz", (req: Request, res: Response): void => {
     const userAntwoord: number = parseInt(req.body.antwoord?.trim()) || 0;
     const isCorrect: boolean = userAntwoord === correctAntwoord;
     const message: string = isCorrect ? "Gefeliciteerd, je hebt het voltooid!" : "Fout! 😢 Probeer opnieuw.";
+    res.render("result", { title: "Challenge resultaat", boodschap: message });
 
     const usernameInputValue: string = req.body.username?.trim() || "";    
     if(isCorrect) {
         postFetch(usernameInputValue);
     }
-    
-    res.render("result", { title: "Challenge resultaat", boodschap: message });
 });
 
 router.get("/news", (req: Request, res: Response): void => {
@@ -40,7 +46,7 @@ router.get("/news", (req: Request, res: Response): void => {
 //redirect get request op /quiz naar /
 router.get("/quiz", (req: Request, res: Response): void => {
     res.redirect("/");
-  });
+});
 
 
 
